208c08666e7527d46eb8474168158c69b8e874a7
Nguyen Long <longnguyenhoang.ict@gmail.com>
2023-08-16 10:07:56 +0700

    remove unused file

--- split_img.py
+++
@@ -1,9 +0,0 @@
-import cv2
-vidcap = cv2.VideoCapture('8473455552405621352.mp4')
-success,image = vidcap.read()
-count = 0
-while success:
-  cv2.imwrite("imgs/frame%d.jpg" % count, image)     # save frame as JPEG file      
-  success,image = vidcap.read()
-  print('Read a new frame: ', success)
-  count += 1
